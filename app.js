e0f7c8f38785dbcc32bdf51a5cf1f5250a053759
Annie Elizabeth Freeman <Annie@Annies-MacBook-Air.local>
2019-08-24 19:02:16 +1200

    Add functionality and style

--- app.js
+++ app.js
@@ -2,71 +2,57 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/views/date.js");
+
 
 const app = express();
+
+const items = ["Buy food", "Edit podcast"];
+const workItems = [];
+
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(express.static("public"));
+
 
 app.get("/", function(req, res) {
 
-  var today = new Date();
-  var currentDay = today.getDay();
-  var day = ""
-
-  switch (currentDay) {
-    case 0:
-      day = "Sunday"
-
-      break;
-    case 1:
-      day = "Monday"
-
-      break;
-    case 2:
-      day = "Tuesday"
-
-      break;
-    case 3:
-      day = "Wednesday"
-
-      break;
-    case 4:
-      day = "Thursday"
-
-      break;
-    case 5:
-      day = "Friday"
-
-      break;
-    case 5:
-      day = "Saturday"
-
-      break;
-
-    default:
-    console.log("Error: current day is equal to " + currentDay);
-
-  }
-
-  if (currentDay === 1) {
-    day = "Monday";
-  } else if (currentDay === 2) {
-    day = "Tuesday";
-  } else if (currentDay === 3) {
-    day = "Wednesday";
-  } else if (currentDay === 4) {
-    day = "Thursday";
-  } else if (currentDay === 5) {
-    day = "Friday";
-  } else if (currentDay === 6) {
-    day = "Saturday";
-  } else {
-    day = "Sunday";
-  }
+const day = date.getDate();
 
 
   res.render("list", {
-    kindOfDay: day
-  })
+    listTitle: day,
+    newListItems: items
+  });
+
+});
+
+app.post("/", function(req, res) {
+
+  const item = req.body.newItem;
+
+  if (req.body.list === "Work") {
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
+
+});
+
+app.get("/work", function(req, res) {
+  res.render("list", {
+    listTitle: "Work List",
+    newListItems: workItems
+  });
+});
+
+
+app.get("/about", function(req, res){
+  res.render("about");
 });
 
 
